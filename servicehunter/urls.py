6213af2ca92130ac73e1191d778f6c6e47b59d3f
Elias Coutinho <coutinho.elias@gmail.com>
2016-02-04 21:06:42 -0300

    Meu git

--- servicehunter/urls.py
+++ servicehunter/urls.py
@@ -13,10 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+
 from django.conf.urls import url
 from django.contrib import admin
+from servicehunter.core import views
 
 urlpatterns = [
     url(r'^$', 'servicehunter.core.views.home'),
     url(r'^admin/', admin.site.urls),
+    url(r'^register/$', views.register, name='register'),
 ]
+
